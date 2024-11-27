2caa8133b99f396023da401fcf3df1a3489228b1
TenJer21 <92504706+TenJer21@users.noreply.github.com>
2024-11-27 21:51:57 +0800

    Sup3 Complete

--- src/components/SensorData.vue
+++ src/components/SensorData.vue
@@ -54,4 +54,53 @@
     },
   };
   </script>
+  <style scoped>
+  body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f9f9f9; /* Light background */
+      margin: 0;
+      padding: 20px;
+  }
   
+  div {
+      max-width: 800px;
+      margin: auto;
+      padding: 20px;
+      background-color: #c357ee; /* White background */
+      border-radius: 8px;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  h1 {
+      color: #ffffff; /* Dark text */
+      font-size: 2em;
+      margin-bottom: 10px;
+  }
+  
+  h2 {
+      color: #ffffff; /* Dark text */
+      font-size: 0.75em;
+      margin-bottom: 10px;
+  }
+  p {
+      color: #ffffff; /* Medium grey for paragraph */
+      font-size: 1.1em;
+  }
+  
+  ul {
+      list-style-type: none;
+      padding: 0;
+  }
+  
+  li {
+      background-color: #e0b7fc; /* Light blue */
+      border-radius: 5px;
+      padding: 10px;
+      margin-bottom: 10px;
+      color: #000; /* Ensure text inside list items is black */
+  }
+  
+  li:hover {
+      background-color: #d1e7fd; /* Darker blue on hover */
+  }
+  </style>
